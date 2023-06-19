3e6cdb726f19cdbe8cffe053ca5d946b44072991
Amal S <amalsunu5469@gmail.com>
2023-06-19 23:47:32 +0530

    add delete and clear all functions completed

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const addForm = document.querySelector(".add");
 
 const tasks = document.querySelector(".tasks")
 
+const clearAll = document.querySelector(".clear")
+
+const messageSpan = document.querySelector(".message-box span")
+
+function updateMessage(){
+    const taskLength = tasks.children.length;
+    messageSpan.textContent = `you have ${taskLength} pending length`;
+    
+}
+updateMessage();
 
 addForm.addEventListener("submit", event => {
     event.preventDefault();
     // remove whitespace
     const value=addForm.task.value.trim();
-
 
     if(value.length){
         
@@ -16,7 +25,24 @@ addForm.addEventListener("submit", event => {
                                 <i class="bi bi-trash3-fill delete"></i>
                             </li>`;
         addForm.reset();
+        updateMessage();
     }
-    
-    
+})
+
+// delete
+
+tasks.addEventListener("click", event =>{
+    if(event.target.classList.contains("delete")){
+        event.target.parentElement.remove();
+        updateMessage();
+    }
+})
+
+
+clearAll.addEventListener("click", event =>{
+    const taskItems = tasks.querySelectorAll("li")
+    taskItems.forEach(item => {
+        item.remove();
+        updateMessage();
+    })
 })
