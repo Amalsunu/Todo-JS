e2c67d33291b1212a8b37a76dee30a651ef56469
Amal S <amalsunu5469@gmail.com>
2023-06-19 16:39:29 +0530

    added js & can read add tasks

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hey')
+const addForm = document.querySelector(".add");
+
+const tasks = document.querySelector(".tasks")
+
+
+addForm.addEventListener("submit", event => {
+    event.preventDefault();
+    // remove whitespace
+    const value=addForm.task.value.trim();
+
+
+    if(value.length){
+        
+        tasks.innerHTML += `<li>
+                                <span>${value}</span>
+                                <i class="bi bi-trash3-fill delete"></i>
+                            </li>`;
+        addForm.reset();
+    }
+    
+    
+})
